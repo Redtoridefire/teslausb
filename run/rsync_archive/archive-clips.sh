ef293f2b8257e7f9bfa58709712a48d84756ecaa
Paul Gardiner <pxgardiner@gmail.com>
2019-06-10 00:42:54 -0400

    added loop to keep car alive while rsync is running

--- run/rsync_archive/archive-clips.sh
+++ run/rsync_archive/archive-clips.sh
@@ -1,5 +1,27 @@
 #!/bin/bash -eu
 
+# Call wake_up_vehicle Tesla API every 120 seconds
+# as long as rsync is running. This has to be run
+# in the background.  
+function keepVehicleAwake() {
+  if [ -f /root/bin/tesla_api.py ]
+    then
+    while :
+    do
+      sleep 120
+      PROC_COUNT=$(pgrep -c rsync)
+      if [ "$PROC_COUNT" == "0" ]
+      then
+        log "Rsync done. Exiting wake_up_vehicle loop."
+        break
+      else
+        /root/bin/tesla_api.py wake_up_vehicle >> "$LOG_FILE"
+      fi
+    done
+  fi
+}
+keepVehicleAwake&
+
 log "Archiving through rsync..."
 
 source /root/.teslaCamRsyncConfig
