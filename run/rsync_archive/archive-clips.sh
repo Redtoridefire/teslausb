e63528daf824069e9fe5d7ff73411353ee35e05e
Paul Gardiner <pxgardiner@gmail.com>
2019-06-26 20:31:25 -0400

    fixed deleting empty dirs

--- run/rsync_archive/archive-clips.sh
+++ run/rsync_archive/archive-clips.sh
@@ -5,12 +5,12 @@ log "Archiving through rsync..."
 source /root/.teslaCamRsyncConfig
 
 num_files_moved=$(rsync -auvh --timeout=45 --remove-source-files --no-perms --stats --log-file=/tmp/archive-rsync-cmd.log $CAM_MOUNT/TeslaCam/saved* $CAM_MOUNT/TeslaCam/SavedClips/* $user@$server:$path | awk '/files transferred/{print $NF}')
-
+log "Deleting empty directories from SavedClips"
 /root/bin/send-push-message "$num_files_moved"
 
 if (( $num_files_moved > 0 ))
 then
-  find $CAM_MOUNT/Teslacam/SavedClips/ -depth -type d -empty -exec rmdir "{}" \;
+  find $CAM_MOUNT/TeslaCam/SavedClips -depth -type d -empty -delete
   log "Successfully synced files through rsync."
 else
   log "No files to archive through rsync."
