edaaa07fb2b09cd3d64843a7c35eef098a898923
Paul Gardiner <pxgardiner@gmail.com>
2019-06-26 21:33:57 -0400

    backing out previous change

--- run/rsync_archive/archive-clips.sh
+++ run/rsync_archive/archive-clips.sh
@@ -5,11 +5,12 @@ log "Archiving through rsync..."
 source /root/.teslaCamRsyncConfig
 
 num_files_moved=$(rsync -auvh --timeout=45 --remove-source-files --no-perms --stats --log-file=/tmp/archive-rsync-cmd.log $CAM_MOUNT/TeslaCam/saved* $CAM_MOUNT/TeslaCam/SavedClips/* $user@$server:$path | awk '/files transferred/{print $NF}')
+
 /root/bin/send-push-message "$num_files_moved"
 
 if (( $num_files_moved > 0 ))
 then
-  find $CAM_MOUNT/TeslaCam/SavedClips -depth -type d -empty -delete
+  find $CAM_MOUNT/Teslacam/SavedClips/ -depth -type d -empty -exec rmdir "{}" \;
   log "Successfully synced files through rsync."
 else
   log "No files to archive through rsync."
