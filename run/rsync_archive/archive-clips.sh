fb2515f74c4b9d9ad6a0017dc2cc3a1c00728258
Paul Gardiner <pxgardiner@gmail.com>
2019-06-26 20:33:42 -0400

    removing log message

--- run/rsync_archive/archive-clips.sh
+++ run/rsync_archive/archive-clips.sh
@@ -5,7 +5,6 @@ log "Archiving through rsync..."
 source /root/.teslaCamRsyncConfig
 
 num_files_moved=$(rsync -auvh --timeout=45 --remove-source-files --no-perms --stats --log-file=/tmp/archive-rsync-cmd.log $CAM_MOUNT/TeslaCam/saved* $CAM_MOUNT/TeslaCam/SavedClips/* $user@$server:$path | awk '/files transferred/{print $NF}')
-log "Deleting empty directories from SavedClips"
 /root/bin/send-push-message "$num_files_moved"
 
 if (( $num_files_moved > 0 ))
