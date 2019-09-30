84aafd37ca9f31058f6c46ff569517026fddf68c
Rob Palkowski <dopeghoti@gmail.com>
2019-09-29 19:08:36 -0700

    Add tool to force archiveloop to sync archives

--- run/force_sync.sh
+++ run/force_sync.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -eu
 
 printf "Forcing archiveloop to sync by pretending to take archive host offline and back online.\n"
-printf "Setting archive unreachable.."
-if ! touch /tmp/archive-is-unreachable; then
-   printf "Something went wrong!  Error %d\nAborting.\n" "$?" 1>&2
-   exit 1
+printf "Setting archive unreachable..\n"
+if ! touch /tmp/archive-is-unreachable
+then
+  printf "Something went wrong!  Error %d\nAborting.\n" "$?" 1>&2
+  exit 1
 fi
-while [[ -f /tmp/archive-is-unreachable ]]; do
-   printf "Waiting for archiveloop to see canary file.."
-   sleep 5
+while [[ -f /tmp/archive-is-unreachable ]]
+do
+  printf "Waiting for archiveloop to see canary file..\n"
+  sleep 5
 done
-printf "Setting archive reachable.."
-if ! touch /tmp/archive-is-reachable; then
-   printf "Something went wrong!  Error %d\nAborting.\n" "$?" 1>&2
-   exit 1
+printf "Setting archive reachable..\n"
+if ! touch /tmp/archive-is-reachable
+then
+  printf "Something went wrong!  Error %d\nAborting.\n" "$?" 1>&2
+  exit 1
 fi
 printf "Done!  archiveloop process should now start its sync process automagically.\n"
 
