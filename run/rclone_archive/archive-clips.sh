8d6e38e60176e8cfe7b9eaeb8373c46230f0921b
jspv <jspvgithub@twinleaf.xyz>
2020-03-03 14:17:07 -0500

    Removed debugging

--- run/rclone_archive/archive-clips.sh
+++ run/rclone_archive/archive-clips.sh
@@ -2,15 +2,6 @@
 
 source /root/.teslaCamRcloneConfig
 
-function send_completed_sns () {
-  log "Sending SNS message $1 for completed upload."
-
-  source /root/.teslaCamSNSTopicARN
-
-  # shellcheck disable=SC2154
-  python3 /root/bin/send_sns.py -t "$sns_topic_arn" -s "TeslaCam Upload" -m "$1"
-}
-
 log "Moving clips to rclone archive..."
 
 FILE_COUNT=$(cd "$CAM_MOUNT"/TeslaCam && find . -maxdepth 3 -path './SavedClips/*' -type f -o -path './SentryClips/*' -type f | wc -l)
@@ -27,12 +18,10 @@ do
         rclone --config /root/.config/rclone/rclone.conf move "$clipfolder/$folderbase" "$drive:$path/$clipfolderbase/$folderbase" --create-empty-src-dirs --delete-empty-src-dirs >> "$LOG_FILE" 2>&1 || true
         rmdir "$folder"
         /root/bin/send-push-message "TeslaUSB:" "Uploaded $clipfolderbase/$folderbase" || true
-        [ -r "/root/.teslaCamSNSTopicARN" ] && send_completed_sns "$clipfolderbase/$folderbase"
       fi
     done
   fi
 done
-
 
 FILES_REMAINING=$(cd "$CAM_MOUNT"/TeslaCam && find . -maxdepth 3 -path './SavedClips/*' -type f -o -path './SentryClips/*' -type f | wc -l)
 NUM_FILES_MOVED=$((FILE_COUNT-FILES_REMAINING))
