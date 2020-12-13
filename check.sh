3e10ff4f80bba495a21738f27085710f59ace6d3
Daniel Nowacki <dandan@Nowackis-MBP.pacanka.local>
2020-12-12 22:07:53 -0500

    make changes to check.sh to enhance shellcheck

--- check.sh
+++ check.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 
-shopt -s globstar nullglob
+# SC1091 - Don't complain about not being able to find files that don't exist.
+
+EXCLUSIONS=SC1091
 
 # print shellcheck version so we know what Github uses
 shellcheck -V
 
-# SC1091 - Don't complain about not being able to find files that don't exist.
-shellcheck --exclude=SC1091 \
+shellcheck --exclude=$EXCLUSIONS \
            ./setup/pi/setup-teslausb \
            ./pi-gen-sources/00-teslausb-tweaks/files/rc.local \
            ./run/archiveloop \
            ./run/auto.teslausb \
            ./run/remountfs_rw \
            ./run/send-push-message \
-           ./run/waitforidle \
-           ./**/*.{sh,ksh,bash}
+           ./run/waitforidle
+
+find . -type f -name '*.sh' -print0 | xargs -0 shellcheck --exclude=$EXCLUSIONS --external-sources "$@" || true
