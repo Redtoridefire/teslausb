2b63be117491b49dd9490361e4a93bccbabd92f0
Malte Bögershausen <malteb@gmail.com>
2020-12-27 14:35:25 +0100

    Add SHARE_DOMAIN for CIFS.
    Fix condition.

--- run/cifs_archive/write-archive-configs-to.sh
+++ run/cifs_archive/write-archive-configs-to.sh
@@ -5,7 +5,7 @@ FILE_PATH="$1"
 (
   echo "username=$SHARE_USER"
   echo "password=$SHARE_PASSWORD"
-  if [ -n "$SHARE_DOMAIN" ]
+  if [ -n "${SHARE_DOMAIN+x}" ]
   then
     echo "domain=$SHARE_DOMAIN"
   fi
